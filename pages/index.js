f7674a9be94feca2e03a04796f00ad99f55a43d3
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2022-03-12 14:10:18 +0100

    add cards

--- pages/index.js
+++ pages/index.js
@@ -400,6 +400,93 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="pt-20 pb-20 grid grid-flow-col gap-16 justify-center">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <div className="w-full mx-auto flex justify-center">
+          <img
+            class="w-32 h-32"
+            src="/img/logo.png"
+            alt="Sunset in the mountains"
+          />
+          </div>
+          <div class="px-6 py-4 flex flex-col justify-center">
+            <div class="font-bold text-xl mb-2 flex justify-center">Mes services périnat'</div>
+            <p class="text-gray-700 text-justify">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          {/* <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #photography
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #travel
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #winter
+            </span>
+          </div> */}
+        </div>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <div className="w-full mx-auto flex justify-center">
+          <img
+            class="w-32 h-32"
+            src="/img/logo.png"
+            alt="Sunset in the mountains"
+          />
+          </div>
+          <div class="px-6 py-4 flex flex-col justify-center">
+            <div class="font-bold text-xl mb-2 flex justify-center">Mes services périnat'</div>
+            <p class="text-gray-700 text-justify">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          {/* <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #photography
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #travel
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #winter
+            </span>
+          </div> */}
+        </div>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <div className="w-full mx-auto flex justify-center">
+          <img
+            class="w-32 h-32"
+            src="/img/logo.png"
+            alt="Sunset in the mountains"
+          />
+          </div>
+          <div class="px-6 py-4 flex flex-col justify-center">
+            <div class="font-bold text-xl mb-2 flex justify-center">Mes services périnat'</div>
+            <p class="text-gray-700 text-justify">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          {/* <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #photography
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #travel
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #winter
+            </span>
+          </div> */}
+        </div>
+      </div>
+
       <footer></footer>
     </div>
   );
