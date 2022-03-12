922fba3ccb8f0e660f398cbbd604c22307b77778
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2022-03-12 15:06:59 +0100

    add footer

--- pages/index.js
+++ pages/index.js
@@ -400,19 +400,22 @@ export default function Home() {
         </div>
       </div>
 
-
-      <h2 className="mt-24 text-4xl text-center font font-extrabold tracking-tight text-gray-900">Mes accompagnements</h2>
+      <h2 className="mt-24 text-4xl text-center font font-extrabold tracking-tight text-gray-900">
+        Mes accompagnements
+      </h2>
       <div className="pt-20 pb-20 grid grid-flow-col gap-16 justify-center">
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <div className="w-full mx-auto flex justify-center">
-          <img
-            className="w-32 h-32"
-            src="/img/logo.png"
-            alt="Sunset in the mountains"
-          />
+            <img
+              className="w-32 h-32"
+              src="/img/logo.png"
+              alt="Sunset in the mountains"
+            />
           </div>
           <div className="px-6 py-4 flex flex-col justify-center">
-            <div className="font-bold text-xl mb-2 flex justify-center">Mes services périnat'</div>
+            <div className="font-bold text-xl mb-2 flex justify-center">
+              Mes services périnat'
+            </div>
             <p className="text-gray-700 text-justify">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
@@ -433,14 +436,16 @@ export default function Home() {
         </div>
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <div className="w-full mx-auto flex justify-center">
-          <img
-            className="w-32 h-32"
-            src="/img/logo.png"
-            alt="Sunset in the mountains"
-          />
+            <img
+              className="w-32 h-32"
+              src="/img/logo.png"
+              alt="Sunset in the mountains"
+            />
           </div>
           <div className="px-6 py-4 flex flex-col justify-center">
-            <div className="font-bold text-xl mb-2 flex justify-center">Mes services Doula</div>
+            <div className="font-bold text-xl mb-2 flex justify-center">
+              Mes services Doula
+            </div>
             <p className="text-gray-700 text-justify">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
@@ -461,14 +466,16 @@ export default function Home() {
         </div>
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <div className="w-full mx-auto flex justify-center">
-          <img
-            className="w-32 h-32"
-            src="/img/logo.png"
-            alt="Sunset in the mountains"
-          />
+            <img
+              className="w-32 h-32"
+              src="/img/logo.png"
+              alt="Sunset in the mountains"
+            />
           </div>
           <div className="px-6 py-4 flex flex-col justify-center">
-            <div className="font-bold text-xl mb-2 flex justify-center">Service Ohmamacare</div>
+            <div className="font-bold text-xl mb-2 flex justify-center">
+              Service Ohmamacare
+            </div>
             <p className="text-gray-700 text-justify">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
@@ -489,13 +496,88 @@ export default function Home() {
         </div>
       </div>
 
+      <h3 className="mt-24 text-4xl text-center font font-extrabold tracking-tight text-gray-900">
+        Mon approche
+      </h3>
 
-      <h3 className="mt-24 text-4xl text-center font font-extrabold tracking-tight text-gray-900">Mon approche</h3>
+      <p className="text-gray-700 pb-12 pt-12 text-center">
+        Texte sur ma philosophie, mes valeurs, mon approche
+      </p>
 
-      <p className="text-gray-700 pb-12 pt-12 text-center">Texte sur ma philosophie, mes valeurs, mon approche</p>
+      <h4 className="mt-24 text-4xl text-center font font-extrabold tracking-tight text-gray-900">
+        Et si on apprenait à se connaitre ?
+      </h4>
 
+      <p className="text-gray-700 pb-12 pt-12 text-center">
+        Retrouvez moi sur Instagram et Facebook pour suivre mon quotidien
+      </p>
 
-      <footer></footer>
+      <footer class="text-center text-white border-t border-t-gray-200">
+        <div class="container mx-auto pt-9">
+          <div class="flex w-full justify-center mb-9">
+            <a href="#!" class="mr-9 text-gray-800">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="facebook-f"
+                class="svg-inline--fa fa-facebook-f w-4"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+                ></path>
+              </svg>
+            </a>
+            <a href="#!" class="mr-9 text-gray-800">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="instagram"
+                class="svg-inline--fa fa-instagram w-6"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+                ></path>
+              </svg>
+            </a>
+            <a href="#!" class="mr-9 text-gray-800">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="linkedin-in"
+                class="svg-inline--fa fa-linkedin-in w-6"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        <div
+          class="text-center text-gray-700 p-4"
+        >
+          © 2022 Copyright:{' '}
+          <a class="text-gray-800" href="https://tailwind-elements.com/">
+            Naitre ensemble
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
