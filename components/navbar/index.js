dd697131ab01b1b3a50ca3a0a7b5115aabc3d872
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2022-03-13 16:48:26 +0100

    add contact page

---
+++ components/navbar/index.js
@@ -0,0 +1,308 @@
+import Head from "next/head";
+import Image from "next/image";
+import React, { useState } from "react";
+
+export default function Navbar() {
+  const [menuOneIsOpen, setMenuOneIsOpen] = useState(false);
+  const [menuTwoIsOpen, setMenuTwoIsOpen] = useState(false);
+
+  return (
+    <div className="fixed top-0 w-full z-50" style={{ background: "#995b37" }}>
+      <div className="w-full ">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1 pl-4">
+            <a href="/">
+              <span className="sr-only">Naitre ensemble</span>
+              <img
+                className="h-12 w-auto"
+                src="/img/logo-text.png"
+                alt="Naitre ensemble"
+              />
+            </a>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <button
+              type="button"
+              className="rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open menu</span>
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
+          <nav className="hidden md:flex space-x-10">
+            <div className="relative">
+              <button
+                type="button"
+                className="text-white group rounded-md inline-flex items-center text-base font-medium focus:outline-none select-none"
+                aria-expanded="false"
+                onClick={() => {
+                  setMenuOneIsOpen(!menuOneIsOpen);
+                  setMenuTwoIsOpen(false);
+                }}
+              >
+                <span>Pour les familles</span>
+                <svg
+                  className="text-white ml-2 h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+              {menuOneIsOpen ? (
+                <div className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                      <a
+                        href="#"
+                        className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                          />
+                        </svg>
+                        <div className="ml-4">
+                          <p className="text-base font-medium text-gray-900">
+                            Mes services périnat
+                          </p>
+                          <p className="mt-1 text-sm text-gray-500">
+                            Get a better understanding of where your traffic is
+                            coming from.
+                          </p>
+                        </div>
+                      </a>
+
+                      <a
+                        href="#"
+                        className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                          />
+                        </svg>
+                        <div className="ml-4">
+                          <p className="text-base font-medium text-gray-900">
+                            Mes services Doula
+                          </p>
+                          <p className="mt-1 text-sm text-gray-500">
+                            Speak directly to your customers in a more
+                            meaningful way.
+                          </p>
+                        </div>
+                      </a>
+
+                      <a
+                        href="#"
+                        className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                          />
+                        </svg>
+                        <div className="ml-4">
+                          <p className="text-base font-medium text-gray-900">
+                            Service Ohmamacare
+                          </p>
+                          <p className="mt-1 text-sm text-gray-500">
+                            Your customers&#039; data will be safe and secure.
+                          </p>
+                        </div>
+                      </a>
+                    </div>
+                    <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+                      <div className="flow-root">
+                        <a
+                          href="#"
+                          className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
+                        >
+                          <svg
+                            className="flex-shrink-0 h-6 w-6 text-gray-400"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                            />
+                          </svg>
+                          <span className="ml-3">Contact</span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : null}
+            </div>
+            <div className="relative">
+              <button
+                type="button"
+                className="text-white group rounded-md inline-flex items-center text-base font-medium focus:outline-none select-none"
+                aria-expanded="false"
+                onClick={() => {
+                  setMenuOneIsOpen(false);
+                  setMenuTwoIsOpen(!menuTwoIsOpen);
+                }}
+              >
+                <span>Pour les structures</span>
+                <svg
+                  className="text-white ml-2 h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+              {menuTwoIsOpen ? (
+                <div className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                      <a
+                        href="#"
+                        className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                          />
+                        </svg>
+                        <div className="ml-4">
+                          <p className="text-base font-medium text-gray-900">
+                            Ateliers parents enfants
+                          </p>
+                          <p className="mt-1 text-sm text-gray-500">
+                            Get a better understanding of where your traffic is
+                            coming from.
+                          </p>
+                        </div>
+                      </a>
+
+                      <a
+                        href="#"
+                        className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                          />
+                        </svg>
+                        <div className="ml-4">
+                          <p className="text-base font-medium text-gray-900">
+                            Sensibilitations pro
+                          </p>
+                          <p className="mt-1 text-sm text-gray-500">
+                            Speak directly to your customers in a more
+                            meaningful way.
+                          </p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              ) : null}
+            </div>
+            <a href="#" className="text-base font-medium text-white">
+              {" "}
+              Votre parole{" "}
+            </a>
+
+            <a href="#" className="text-base font-medium text-white">
+              {" "}
+              Bon cadeau{" "}
+            </a>
+
+            <a href="/contact" className="text-base font-medium text-white">
+              {" "}
+              Contact{" "}
+            </a>
+          </nav>
+          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
 import React, { useState } from "react";
+import Navbar from "../components/navbar";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="relative">
+      <Head>
+        <title>Naitre ensemble - contact</title>
+        <meta name="description" content="Naitre ensemble / contact" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+    </div>
+  );
 }
