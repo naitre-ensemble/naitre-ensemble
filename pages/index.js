d3a0e76c3df365f9fef4956951dd6500c0ee23cd
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2022-03-13 14:28:24 +0100

    add menu for structure

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 export default function Home() {
 
   const [menuOneIsOpen, setMenuOneIsOpen] = useState(false);
+  const [menuTwoIsOpen, setMenuTwoIsOpen] = useState(false);
 
   return (
     <div className="relative">
@@ -51,9 +52,12 @@ export default function Home() {
               <div className="relative">
                 <button
                   type="button"
-                  className="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none select-none"
                   aria-expanded="false"
-                  onClick={() => setMenuOneIsOpen(!menuOneIsOpen)}
+                  onClick={() => {
+                    setMenuOneIsOpen(!menuOneIsOpen);
+                    setMenuTwoIsOpen(false);
+                  }}
                 >
                   <span>Pour les familles</span>
                   <svg
@@ -191,14 +195,97 @@ export default function Home() {
                   </div>
                 </div> : null }
               </div>
+              <div className="relative">
+              <button
+                  type="button"
+                  className="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none select-none"
+                  aria-expanded="false"
+                  onClick={() => {
+                    setMenuOneIsOpen(false);
+                    setMenuTwoIsOpen(!menuTwoIsOpen)
+                  }}
+                >
+                  <span>Pour les structures</span>
+                <svg
+                    className="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+              </button>
+              { menuTwoIsOpen ? <div className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                      <a
+                        href="#"
+                        className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                          />
+                        </svg>
+                        <div className="ml-4">
+                          <p className="text-base font-medium text-gray-900">
+                            Ateliers parents enfants
+                          </p>
+                          <p className="mt-1 text-sm text-gray-500">
+                            Get a better understanding of where your traffic is
+                            coming from.
+                          </p>
+                        </div>
+                      </a>
 
-              <a
-                href="#"
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
-                {" "}
-                Pour les structures{" "}
-              </a>
+                      <a
+                        href="#"
+                        className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                          />
+                        </svg>
+                        <div className="ml-4">
+                          <p className="text-base font-medium text-gray-900">
+                            Sensibilitations pro
+                          </p>
+                          <p className="mt-1 text-sm text-gray-500">
+                            Speak directly to your customers in a more
+                            meaningful way.
+                          </p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div> : null }
+              </div>
               <a
                 href="#"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
