d09edf1cd836c0c0cdf37c0507eba4ae9c7d15fb
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2022-05-26 19:13:17 +0200

    change homepage

---
+++ pages/concept.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import React, { useState } from "react";
+import Navbar from "../components/navbar";
+
+export default function WhoIam() {
+  return (
+    <div className="relative">
+      <Head>
+        <title>Naitre ensemble - Concept</title>
+        <meta name="description" content="Naitre ensemble / Concept" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+
+      <div className="w-full pt-36 lg:pt-36 p-4 lg:p-6">
+        <h1
+          style={{ color: "#906c61" }}
+          className="pb-4 lg:pb-0 text-center text-4xl font font-extrabold tracking-tight sm:text-4xl lg:text-left"
+        >
+          Concept
+        </h1>
+      </div>
+    </div>
+  );
+}
